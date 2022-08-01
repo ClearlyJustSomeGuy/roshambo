298c75f421fd7cab4f3a49369ad592fc6600101f
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-01 15:24:47 -0600

    Play five rounds

--- js/jscript.js
+++ js/jscript.js
@@ -1,26 +1,43 @@
+const CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
+let player = 0;
+let computer = 0;
+
+
+//Play 5 rounds
+for (i = 0; i < 5; i++) {
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
+    //Get user's input of rock, paper or scissors, store in playerSelection
+    let playerSelection = prompt('Enter Rock, Paper or Scissors, please:');
+    while (playerSelection.toUpperCase() !== 'ROCK' && playerSelection.toUpperCase() !== 'PAPER' && playerSelection.toUpperCase() !== 'SCISSORS') {
+        playerSelection = prompt('Hmm... why don\'t we try that again. Rock, paper or scissors, please.');
+    }
+    console.log(playerSelection);
+
+    // Determine if win, loss or tie
+    if (playRound(playerSelection, computerSelection) === 'TIE') {
+        console.log('Tie game!');
+    } else if (playRound(playerSelection, computerSelection) === true) {
+        player++;
+        console.log('Congrats! ' + playerSelection.toUpperCase() + ' beats ' + computerSelection + '!');
+    } else if (playRound(playerSelection, computerSelection) === false) {
+        computer++;
+        console.log('Sorry! ' + computerSelection + ' beats ' + playerSelection.toUpperCase() + '!');
+    }
+    console.log('Player: ' + player + '. Computer: ' + computer + '.');
+}
+
+
+
 // Write function getComputerChoice to randomly return 'Rock' 'Paper' or 'Scissor' store as computerSelection
-
-let choices = ['ROCK', 'PAPER', 'SCISSORS'];
-let computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
 function getComputerChoice() {
-    let choice = choices[Math.floor(Math.random() * 3)];
+    let choice = CHOICES[Math.floor(Math.random() * 3)];
     return choice;
 }
 
-//Get user's input of rock, paper or scissors, store in playerSelection
-let playerSelection = prompt('Enter Rock, Paper or Scissors, please:');
 
-while (playerSelection.toUpperCase() !== 'ROCK' && playerSelection.toUpperCase() !== 'PAPER' && playerSelection.toUpperCase() !== 'SCISSORS') {
-    playerSelection = prompt('Hmm... why don\'t we try that again. Rock, paper or scissors, please.');
-}
-
-console.log(playerSelection);
-
-//Write function to compare computerSelection and playerSelection and return win or lose
-
+//Write function to compare computerSelection and playerSelection and return true, false, tie
 function playRound (player, comp) {
     if (player.toUpperCase() === comp) {
         return 'TIE';
@@ -43,14 +60,4 @@ function playRound (player, comp) {
             return true;
         }
     }
-}
-
-
-// Determine if win, loss or tie
-if (playRound(playerSelection, computerSelection) === 'TIE') {
-    alert('Tie game!');
-} else if (playRound(playerSelection, computerSelection) === true) {
-    alert('Congrats! ' + playerSelection.toUpperCase() + ' beats ' + computerSelection + '!');
-} else if (playRound(playerSelection, computerSelection) === false) {
-    alert('Sorry! ' + computerSelection + ' beats ' + playerSelection.toUpperCase() + '!');
 }
