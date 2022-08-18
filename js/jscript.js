a34b1fcc4ed5672f671883df2d5dff5bfa341289
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-18 14:59:39 -0600

    Implement buttons on roshambo

--- js/jscript.js
+++ js/jscript.js
@@ -2,32 +2,17 @@ const CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
 let player = 0;
 let computer = 0;
 
-//game();
 
-getPlayerChoice();
 
+// return player's choice by clicking the buttons
 function getPlayerChoice() {
-    let buttons = document.querySelectorAll('button');
-    console.log(buttons);
+    let buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            alert(button.id);
+            winOrLose(playRound(button.id, getComputerChoice()));
         })
-    })
-        
+        })
     };
-        // Previous implementation used a prompt to get user's input
-        // //Get user's input of rock, paper or scissors, store in playerSelection
-        // let playerSelection = prompt('Enter Rock, Paper or Scissors, please:');
-        // while (playerSelection.toUpperCase() !== 'ROCK' && 
-        //     playerSelection.toUpperCase() !== 'PAPER' && 
-        //     playerSelection.toUpperCase() !== 'SCISSORS') {
-        //     playerSelection = prompt('Hmm... why don\'t we try that again. Rock, paper or scissors, please.');
-        // }
-        // console.log(playerSelection);
-        // return playerSelection;
-
-// }
 
 //getComputerChoice to randomly return 'Rock' 'Paper' or 'Scissor' store as computerSelection
 function getComputerChoice() {
@@ -38,6 +23,8 @@ function getComputerChoice() {
 
 //Write function to compare computerSelection and playerSelection and return true, false, tie
 function playRound (player, comp) {
+    console.log('Play Round player choice is ' + player);
+    console.log('Play Round Comp choice is ' + comp);
     if (player.toUpperCase() === comp) {
         return 'TIE';
     } else if (player.toUpperCase() === 'ROCK') {
@@ -66,33 +53,51 @@ function playRound (player, comp) {
 function winOrLose(result) {
     if (result === 'TIE') {
         alert('Tie game!');
+        updateScore();
     } else if (result === true) {
         player++;
         alert('Congrats! You\'ve won this round!');
+        updateScore();
     } else if (result === false) {
         computer++;
         alert('Sorry! You\'ve lost this round!');
-        //alert('Sorry! ' + computerSelection + ' beats ' + playerSelection.toUpperCase() + '!');
+        updateScore();
     }
     console.log('Player: ' + player + '. Computer: ' + computer + '.');
 }
 
-//Play 5 rounds
-function game() {
-    for (i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        console.log(computerSelection);
-
-        let playerSelection = getPlayerChoice();
-
-        winOrLose(playRound(playerSelection, computerSelection));
-    }
-
-    if (player > computer) {
-        alert('Congrats! You won it all!  Player: ' + player + ' Computer: ' + computer);
-    } else if (computer > player) {
-        alert('Sorry! The computer won it all!  Player: ' + player + ' Computer: ' + computer);
-    } else {
-        alert('Tie game!  Player: ' + player + ' Computer: ' + computer);
-    }
+function updateScore() {
+    let pScore = document.querySelector('.p-score');
+    let cScore = dcoument.querySelector('.c-score');
+    pScore.textContent = player;
+    cScore.textContent = computer;
 }
+
+
+//Play 5 rounds
+// function game() {
+    // for (i = 0; i < 5; i++) {
+    //     let computerSelection = getComputerChoice();
+    //     console.log('Computer\'s selection = ' + computerSelection);
+
+    //     let playerSelection;
+    //     while (playerSelection === undefined) {
+    //         playerSelection = getPlayerChoice();
+    //     }
+    //     console.log('Player selection = ' + playerSelection);
+
+        
+    // }
+
+    // if (player > computer) {
+    //     alert('Congrats! You won it all!  Player: ' + player + ' Computer: ' + computer);
+    // } else if (computer > player) {
+    //     alert('Sorry! The computer won it all!  Player: ' + player + ' Computer: ' + computer);
+    // } else {
+    //     alert('Tie game!  Player: ' + player + ' Computer: ' + computer);
+    // }
+// }
+
+// game();
+
+getPlayerChoice();
